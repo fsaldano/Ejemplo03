a437c420c2ac246845e13920a2146e0ea00f9900
cetecom <cetecom@LCV10808>
2019-03-11 22:36:18 -0300

    Ejemplos Basicos de CSS

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link href="estilos.css" rel="stylesheet" type="text/css"/>        
+        <title>Ejercicio Posición Elementos</title>        
+    </head>
+    <body>
+        <div id="menu">Menu</div>
+        <div id="head">Head</div>
+        <div id="contenedor">Contenedor</div>      
+    </body>
+</html>
